38a9242a5d8dcf95e0055f4a33e18a92fd3215f8
bmordan <bernardmordan@gmail.com>
2019-04-05 09:48:21 +0100

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz num
+    case "#{num%3 == 0}#{num%5 == 0}".gsub(/false/,"-").gsub(/true/,"o")
+    when "o-"
+        "Fizz"
+    when "-o"
+        "Buzz"
+    when "oo"
+        "FizzBuzz"
+    else
+        nil
+    end
+end
